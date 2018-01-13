2792fdbef7439f45634883f69c51835b6028e168
Jovani Harrington <jharrington920@gmail.com>
2018-01-13 05:00:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
 function happyHolidays (String) {
   return "Happy holidays!"
 }
+
+function happyHolidaysTo (string) {
+  if (string) "Happy holidays"; {
+    return `Happy holidays, ${name}!`
+  }
+}
